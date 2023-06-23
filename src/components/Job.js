ef33854c12a09350c6e2b163c324fdd29cfbd3a3
suvash <champsuvash@gmail.com>
2023-06-23 10:38:23 +0545

    added props Job

--- src/components/Job.js
+++ src/components/Job.js
@@ -1,23 +1,24 @@
 import React from "react";
 
-function Job() {
+function Job(props) {
+  const { job } = props;
   return (
     <div className="main">
       <div className="left-part">
-        <img src="" alt="job logo" />
+        <img src={job.company_logo} alt="job logo" />
         <div className="job-details">
           <div className="top-deati">
-            <p className="company"></p>
+            <p className="company">{job.company}</p>
             <div className="job-features"></div>
           </div>
 
-          <h2 className="job-title">Front End</h2>
+          <h2 className="job-title">{job.position}</h2>
 
-            <div className="bottom-details">
-                <p className="job-time"></p>
-                <p className="job-type"></p>
-                <p className="location"></p>
-            </div>
+          <div className="bottom-details">
+            <p className="job-time">{job.posted_on}</p>
+            <p className="job-timing">{job.timing}</p>
+            <p className="location">{job.location}</p>
+          </div>
         </div>
       </div>
       <div className="right-part"></div>
