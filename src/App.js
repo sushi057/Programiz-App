794b6b54c68e43408b6401d21e738e4c5158a176
suvash <champsuvash@gmail.com>
2023-06-24 10:32:12 +0545

    Fixed filter bar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,53 @@
 import "./App.css";
 import Job from "./components/Job";
+import Filter from "./components/Filter";
 import jobLists from "./assets/jobLists";
+import { useState } from "react";
 
 function App() {
+  const [selectKeywords, setSelectKeywords] = useState([]);
+
+  function addKeywords(key) {
+    if (selectKeywords.includes(key)) {
+      setSelectKeywords((prevItem) => {
+        let index = selectKeywords.indexOf(key);
+        const removeKeywords = [...prevItem];
+        removeKeywords.splice(index, 1);
+        return removeKeywords;
+      });
+    } else {
+      setSelectKeywords((prevItem) => [...prevItem, key]);
+    }
+
+    console.log(selectKeywords);
+  }
+
+  function clearKeywords() {
+    setSelectKeywords([]);
+  }
   return (
     <div className="App">
+      <div className="filter-bar">
+        {selectKeywords.length !== 0 && (
+          <div className="key-filter">
+            <div className="filter-words">
+              {selectKeywords.map((item) => (
+                <Filter filterWords={item} />
+              ))}
+            </div>
+            <button
+              className="clear-btn"
+              onClick={() => {
+                clearKeywords();
+              }}
+            >
+              Clear
+            </button>
+          </div>
+        )}
+      </div>
       {jobLists.map((item) => (
-        <Job job={item} />
+        <Job job={item} addKeywords={addKeywords} />
       ))}
     </div>
   );
--- src/components/Job.js
+++ src/components/Job.js
@@ -1,52 +1,11 @@
-import React, { useState } from "react";
-import Filter from "./Filter";
+import React from "react";
 import "../styles/job.css";
 
 function Job(props) {
-  const { job } = props;
-
-  const [selectKeywords, setSelectKeywords] = useState([]);
-
-  function addKeywords(key) {
-    if (selectKeywords.includes(key)) {
-      setSelectKeywords((prevItem) => {
-        let index = selectKeywords.indexOf(key);
-        const removeKeywords = [...prevItem];
-        removeKeywords.splice(index, 1);
-        return removeKeywords;
-      });
-    } else {
-      setSelectKeywords((prevItem) => [...prevItem, key]);
-    }
-
-    console.log(selectKeywords);
-  }
-
-  function clearKeywords() {
-    setSelectKeywords([]);
-  }
+  const { job, addKeywords } = props;
 
   return (
     <div className="main">
-      {/* <div className="filter-bar">
-        {selectKeywords.length !== 0 && (
-          <div className="key-filter">
-            <div className="filter-words">
-              {selectKeywords.map((item) => (
-                <Filter filterWords={item} />
-              ))}
-            </div>
-            <button
-              className="clear-btn"
-              onClick={() => {
-                clearKeywords();
-              }}
-            >
-              Clear
-            </button>
-          </div>
-        )}
-      </div> */}
       <div className="left-part">
         <img src={job.company_logo} alt="job logo" />
         <div className="job-details">
