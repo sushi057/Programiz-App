fbd0c95a0c228a29ea3b82cc47629b0bf65d22e9
suvash <champsuvash@gmail.com>
2023-06-24 08:36:27 +0545

    remove element by X

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -2,10 +2,17 @@ import React from "react";
 
 function Filter(props) {
   const { filterWords } = props;
+
+  function removeItem(el) {
+    let element = el;
+    element.remove();
+  }
   return (
     <div className="filter">
       <div className="filter-key">{filterWords}</div>
-      <div className="crossX">X</div>
+      <div className="crossX" onClick={removeItem(this)}>
+        X
+      </div>
     </div>
   );
 }
